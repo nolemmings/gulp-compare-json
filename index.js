fcb0a8183fd2b4abe871d0ea56cec4e4f4365a7b
Niels Krijger <niels@kryger.nl>
2015-10-19 15:07:16 +0200

    Output is now using gutil.log always

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var through = require('through2');
 module.exports = function(options) {
   var failed = false;
   var opts = options || {
-    failOnError: false,
-    output: gutil.log
+    failOnError: false
   };
+  opts.output = gutil.log
   opts.error = function(msg) {
     if (opts.failOnError) {
       failed = true;
